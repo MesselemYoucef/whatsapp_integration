0cc0731f24b171dcf76afccd209c50546ed0fee5
Youcef Messelem <messelemy@gmail.com>
2021-10-23 11:36:16 +0400

    Whatsapp module initiation

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Whatsapp Integration',
+    'version': '1.0',
+    'summary': 'Send quotation link of a customer via whatsapp link',
+    'sequence': -104,
+    'description': """Send quotation link of a customer via whatsapp link""",
+    'category': 'Productivity',
+    'website': 'https://www.odoo.com',
+    'Licence': 'LGPL-3',
+    'images': [],
+    'depends': [
+        'sale',
+        'product',
+    ],
+    'data': [
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
