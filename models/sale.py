f1e3a39bc87f14a3dc20a3fd029e411188ba456a
Youcef Messelem <messelemy@gmail.com>
2021-10-27 15:16:03 +0400

    Added an initial solution to the domain name

--- models/sale.py
+++ models/sale.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+import socket
 
 # this is a class for the hospital patient
 
@@ -9,10 +10,12 @@ class SalesOrder(models.Model):
     def action_url(self):
         "prepares the link for the customer preview url so he can check his sales order"
         host = "http://localhost:8015/my/orders/"
-        id = self.id
         token = "?access_token="+self.access_token
         link = host + str(id) + token
         message = "Dear%20Customer,%0aYour%20Invoice:%20"+self.name+"%20has%20been%20issued.%0aPlease,%20check%20your%20order%20on%20the%20link%20bellow:%0a"+link
+        hostname = socket.gethostname()
+        domain_name = socket.gethostbyname(hostname)
+        print(f"Your host name is: {domain_name}")
         # You can replace the URL with the url of your DB
         return{
             'type': 'ir.actions.act_url',
