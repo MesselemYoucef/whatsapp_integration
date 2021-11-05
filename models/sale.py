625acd8d8227abda72c4364cf47d61b2041dc2f5
YoucefMesselem <messelemy@gmail.com>
2021-11-05 23:04:45 +0400

    Fixed the access token should be an str issue

--- models/sale.py
+++ models/sale.py
@@ -14,9 +14,9 @@ class SalesOrder(models.Model):
         hostname = socket.gethostname()
         domain_name = socket.gethostbyname(hostname)
         host = f"http://{domain_name}:8015/my/orders/"
-        token = "?access_token="+self.access_token
+        token = "?access_token="+str(self.access_token)
         link = host + str(id) + token
-        print(self.partner_id.mobile)
+        partner_mobile = str(self.partner_id.mobile)
         message = "Dear%20Customer,%0aYour%20Invoice:%20"+self.name+"%20has%20been%20issued.%0aPlease,%20check%20your%20order%20on%20the%20link%20bellow:%0a"+link
         # You can replace the URL with the url of your DB
         for record in self:
@@ -25,5 +25,5 @@ class SalesOrder(models.Model):
         return{
             'type': 'ir.actions.act_url',
             'target': 'new',
-            'url': "https://wa.me/"+self.partner_id.mobile+"?text="+message,
+            'url': "https://wa.me/"+partner_mobile+"?text="+message,
         }
